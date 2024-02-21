85076d38d3cfc576bbe43856f403490d2e2e377b
Flemming <fleeri91@gmail.com>
2024-02-21 19:10:02 +0100

    stryktipset route

---
+++ src/app/api/stryktipset/route.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  try {
+    const response = await axios.get(
+      `https://api.www.svenskaspel.se/external/1/draw/stryktipset/draws?accesskey=${process.env.NEXT_PUBLIC_API_KEY}`
+    );
+    const data = response.data;
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json(error);
+  }
+}
--- src/store/api/stryktipset.ts
+++ src/store/api/stryktipset.ts
@@ -1,17 +1,17 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { StryktipsetRoot } from "../../types/Stryktipset";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { StryktipsetRoot } from '../../types/Stryktipset';
 
 // Define a service using a base URL and expected endpoints
 export const stryktipsetApi = createApi({
-  reducerPath: "stryktipsetApi",
+  reducerPath: 'stryktipsetApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://api.www.svenskaspel.se/external/1/draw/",
+    baseUrl: '/api/',
   }),
-  tagTypes: ["Stryktipset"],
+  tagTypes: ['Stryktipset'],
   endpoints: (builder) => ({
-    getStryktipset: builder.query<StryktipsetRoot, string>({
-      query: (key) => `/europatipset/draws?accesskey=${key}`,
-      providesTags: ["Stryktipset"],
+    getStryktipset: builder.query<StryktipsetRoot, void>({
+      query: () => `stryktipset`,
+      providesTags: ['Stryktipset'],
     }),
   }),
 });
