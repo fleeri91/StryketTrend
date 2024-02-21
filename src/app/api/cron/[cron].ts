190a10bae0318ecf1424cbbe562e825c56ae070f
Flemming <fleeri91@gmail.com>
2024-02-21 17:00:16 +0100

    removed usused files

--- src/app/api/cron/[cron].ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-export default async function handler(req: NextRequest) {
-  const dynamicApiPart = req.nextUrl.pathname.split('/')[3];
-
-  console.log(dynamicApiPart);
-}
