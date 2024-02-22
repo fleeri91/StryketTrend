f16685a567652dee385d4cdf3138d57d42a0d212
Flemming <fleeri91@gmail.com>
2024-02-23 00:17:21 +0100

    date fix

--- src/components/HistoryList/HistoryItem/HistoryItem.tsx
+++ src/components/HistoryList/HistoryItem/HistoryItem.tsx
@@ -1,13 +1,17 @@
+import dayjs from 'dayjs';
+import sv from 'dayjs/locale/sv';
+
 interface HistoryItemProps {
+  time?: string;
   onClick?: () => void;
 }
 
-const HistoryItem = ({ onClick }: HistoryItemProps): JSX.Element => (
+const HistoryItem = ({ time, onClick }: HistoryItemProps): JSX.Element => (
   <li
     onClick={onClick}
     className='cursor-pointer text-center border font-bold rounded-md p-4 hover:bg-white hover:text-slate-950 transition-all'
   >
-    Torsdag 12:00
+    {dayjs(time).locale(sv).format('dddd hh:mm')}
   </li>
 );
 
