b117b587deb83c68aa2b923bec8349ac67218e55
Flemming <fleeri91@gmail.com>
2024-02-22 23:36:24 +0100

    size prop

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,14 +1,26 @@
 import { Fragment } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
+import clsx from 'clsx';
+
+import type { Size } from 'types/Tailwind';
 
 interface ModalProps {
   title?: string;
   children: React.ReactNode;
+  width?: Size;
   isOpen: boolean;
   onClose: () => void;
 }
 
-const Modal = ({ title, children, isOpen, onClose }: ModalProps) => {
+const Modal = ({ title, children, width, isOpen, onClose }: ModalProps) => {
+  const SizeMapper = {
+    '2xl': 'max-w-screen-2xl',
+    xl: 'max-w-screen-xl',
+    lg: 'max-w-screen-lg',
+    md: 'max-w-screen-md',
+    sm: 'max-w-screen-sm',
+  };
+
   return (
     <Transition appear show={isOpen} as={Fragment}>
       <Dialog as='div' className='relative z-10' onClose={onClose}>
@@ -35,7 +47,12 @@ const Modal = ({ title, children, isOpen, onClose }: ModalProps) => {
               leaveFrom='opacity-100 scale-100'
               leaveTo='opacity-0 scale-95'
             >
-              <Dialog.Panel className='dark:bg-gray-950 dark:text-white w-full transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all'>
+              <Dialog.Panel
+                className={clsx(
+                  'dark:bg-gray-950 dark:text-white transform overflow-hidden w-full rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all',
+                  width && SizeMapper[width]
+                )}
+              >
                 <Dialog.Title
                   as='h3'
                   className='text-2xl font-medium text-center leading-6'
