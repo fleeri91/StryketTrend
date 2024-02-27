672dae4936550b51bc0b08ba2e81b3d0ad82ee71
Flemming <fleeri91@gmail.com>
2024-02-27 17:25:46 +0100

    adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -18,6 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <Script
+          async
+          src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1299064064705433'
+          crossOrigin='anonymous'
+        ></Script>
+      </head>
       <body className={inter.className}>
         <ReduxProvider>{children}</ReduxProvider>
       </body>
