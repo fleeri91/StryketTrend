31912c2e9a066f5302ddb92a25d2d3b01e099fe3
Flemming Eriksson <fleeri91@gmail.com>
2024-03-02 20:12:01 +0100

    adsense disable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,36 @@
-import Script from 'next/script';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
+import Script from 'next/script'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-import ReduxProvider from '@store/provider';
+import ReduxProvider from '@store/provider'
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Stryket Trend',
   description: '',
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
+        {/*
         <Script
           async
           src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1299064064705433'
           crossOrigin='anonymous'
         ></Script>
+        */}
       </head>
       <body className={inter.className}>
         <ReduxProvider>{children}</ReduxProvider>
       </body>
     </html>
-  );
+  )
 }
