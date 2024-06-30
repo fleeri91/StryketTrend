57a09ad172a862aa0efe8cc4b6871346deb1cefc
Flemming <fleeri91@gmail.com>
2024-07-01 00:36:18 +0200

    no events

---
+++ src/components/NoEvents/NoEvents.tsx
@@ -0,0 +1,7 @@
+const NoEvents = (): JSX.Element => (
+  <div className="flex justify-center items-center h-screen">
+    <h1 className="text-2xl">Stryktipset har inte öppnat</h1>
+  </div>
+)
+
+export default NoEvents
