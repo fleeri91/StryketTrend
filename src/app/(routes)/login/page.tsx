0287f774e4793fd8c52d00f9d8e5fbd8de529ee2
Flemming <fleeri91@gmail.com>
2024-07-01 12:59:28 +0200

    fix

--- src/app/(routes)/login/page.tsx
+++ src/app/(routes)/login/page.tsx
@@ -1,8 +1,6 @@
 import LoginForm from '@components/LoginForm'
-import { getServerSession } from 'next-auth'
-import { redirect } from 'next/navigation'
 
-const LoginPage = async () => {
+const LoginPage = () => {
   return <LoginForm />
 }
 
